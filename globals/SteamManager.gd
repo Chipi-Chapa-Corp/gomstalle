7713cd9cc8fa7842a37020db09ba9bb71e3dc9ed
Artem Prokop <artem13.prokop@gmail.com>
2025-09-13 00:41:55 +0200

    feat: steam

---
+++ globals/SteamManager.gd
@@ -0,0 +1,127 @@
+extends Node
+
+var _ready_ok: bool = false
+var current_lobby_id: int = 0
+
+const PACKET_READ_LIMIT: int = 32
+const MAX_MEMBERS: int = 10
+
+# --------- PUBLIC API ---------
+signal lobby_created(error)
+signal lobby_joined(error)
+signal lobby_match_list_updated(lobbies: Array)
+
+func is_ready() -> bool:
+	return _ready_ok
+
+func join_lobby(lobby_id: int) -> void:
+	print("Attempting to join lobby %s" % lobby_id)
+	Steam.joinLobby(lobby_id)
+
+func create_lobby() -> void:
+	Steam.createLobby(Steam.LOBBY_TYPE_PUBLIC, MAX_MEMBERS)
+
+func refresh_lobby_list() -> void:
+	Steam.addRequestLobbyListDistanceFilter(Steam.LOBBY_DISTANCE_FILTER_WORLDWIDE)
+	Steam.addRequestLobbyListStringFilter("_app_id", "gomstalle", Steam.LOBBY_COMPARISON_EQUAL)
+	Steam.requestLobbyList()
+# --------- ---------- ---------
+
+func _ready() -> void:
+	if not Steam:
+		push_error("Steam singleton not found")
+		return
+	print("Steam singleton Found")
+	
+	var ok = Steam.steamInit()
+	if ok:
+		_listen()
+		_check_command_line()
+		set_process(true)
+		_ready_ok = true
+		print("Steam API initialized as user " + Steam.getPersonaName())
+	else:
+		push_error("Error: Failed to initialize Steam API")
+
+func _process(_delta: float) -> void:
+	if Steam and Steam.has_method("run_callbacks"):
+		Steam.run_callbacks()
+
+func _listen():
+	Steam.lobby_created.connect(_on_lobby_created)
+	Steam.lobby_joined.connect(_on_lobby_joined)
+	Steam.lobby_match_list.connect(_on_lobby_match_list)
+	Steam.p2p_session_request.connect(_on_p2p_session_request)
+
+func _check_command_line() -> void:
+	var args := OS.get_cmdline_args()
+	if args.size() > 0:
+		if args[0] == "+connect_lobby":
+			if int(args[1]) > 0:
+				print("Command line lobby ID: %s" % args[1])
+				# join_lobby(int(args[1]))
+
+func _on_lobby_created(lobby_connect: int, lobby_id: int) -> void:
+	if lobby_connect != 1:
+		lobby_created.emit("Failed to create lobby: " + str(lobby_connect))
+		return
+		
+	current_lobby_id = lobby_id
+	Steam.setLobbyJoinable(lobby_id, true)
+	Steam.setLobbyData(lobby_id, "_app_id", "gomstalle")
+	Steam.setLobbyData(lobby_id, "name", "Test Name")
+	Steam.setLobbyData(lobby_id, "mode", "Test")
+
+	var set_relay := Steam.allowP2PPacketRelay(true)
+	print("Set relay: %s" % set_relay)
+
+	print("Lobby created: %s" % lobby_id)
+	lobby_created.emit(null)
+
+func _on_lobby_match_list(lobby_ids: Array) -> void:
+	var list = lobby_ids.map(func(lobby_id: int):
+		var lobby_name := Steam.getLobbyData(lobby_id, "name")
+		var lobby_mode := Steam.getLobbyData(lobby_id, "mode")
+		var lobby_num_members := Steam.getNumLobbyMembers(lobby_id)
+		return {
+			"id": lobby_id,
+			"name": lobby_name,
+			"mode": lobby_mode,
+			"num_members": lobby_num_members
+		}
+	)
+	lobby_match_list_updated.emit(list)
+
+func _on_lobby_joined(lobby_id: int, _permissions: int, _locked: bool, response: int) -> void:
+	if response == Steam.CHAT_ROOM_ENTER_RESPONSE_SUCCESS:
+		current_lobby_id = lobby_id
+		_make_p2p_handshake()
+		lobby_joined.emit(null)
+		print("Lobby joined (should've): %s" % lobby_id)
+	else:
+		print("Failed to join lobby: %s" % response)
+		var fail_reason: String
+
+		match response:
+			Steam.CHAT_ROOM_ENTER_RESPONSE_DOESNT_EXIST: fail_reason = "This lobby no longer exists."
+			Steam.CHAT_ROOM_ENTER_RESPONSE_NOT_ALLOWED: fail_reason = "You don't have permission to join this lobby."
+			Steam.CHAT_ROOM_ENTER_RESPONSE_FULL: fail_reason = "The lobby is now full."
+			Steam.CHAT_ROOM_ENTER_RESPONSE_ERROR: fail_reason = "Uh... something unexpected happened!"
+			Steam.CHAT_ROOM_ENTER_RESPONSE_BANNED: fail_reason = "You are banned from this lobby."
+			Steam.CHAT_ROOM_ENTER_RESPONSE_LIMITED: fail_reason = "You cannot join due to having a limited account."
+			Steam.CHAT_ROOM_ENTER_RESPONSE_CLAN_DISABLED: fail_reason = "This lobby is locked or disabled."
+			Steam.CHAT_ROOM_ENTER_RESPONSE_COMMUNITY_BAN: fail_reason = "This lobby is community locked."
+			Steam.CHAT_ROOM_ENTER_RESPONSE_MEMBER_BLOCKED_YOU: fail_reason = "A user in the lobby has blocked you from joining."
+			Steam.CHAT_ROOM_ENTER_RESPONSE_YOU_BLOCKED_MEMBER: fail_reason = "A user you have blocked is in the lobby."
+
+		refresh_lobby_list()
+		lobby_joined.emit(fail_reason)
+
+func _make_p2p_handshake() -> void:
+	var host_id := Steam.getLobbyOwner(current_lobby_id)
+	print("Sending P2P handshake to the lobby %s to %s" % [current_lobby_id, host_id])
+	Steam.sendP2PPacket(host_id, PackedByteArray([1]), Steam.P2P_SEND_RELIABLE, 0)
+
+func _on_p2p_session_request(peer_id: int, _session_request_flags: int) -> void:
+	print("Accepted P2P handshake to the lobby from %s" % peer_id)
+	Steam.acceptP2PSessionWithUser(peer_id)
--- scenes/main/script.gd
+++ scenes/main/script.gd
@@ -1,54 +1,58 @@
 extends NinePatchRect
 
-@onready var game_scene: PackedScene = preload("res://scenes/world/scene.tscn")
+@onready var world_scene: PackedScene = preload("res://scenes/world/scene.tscn")
+
+@onready var MainSection = $Main
+@onready var JoinSection = $Join
+@onready var JoinLobbyList = $Join/List/Scrollable/Container
+@onready var JoinSearching = $Join/Searching
+
+func _on_join_pressed() -> void:
+	SteamManager.lobby_match_list_updated.connect(_set_rooms)
+	SteamManager.refresh_lobby_list()
+	JoinSection.visible = true
+	MainSection.visible = false
 
 func _on_host_pressed() -> void:
 	Settings.is_host = true
-	_on_start()
-
-func _on_join_room(room) -> void:
-	var on_join_reply = func(ok: bool, net_id: int, host: Dictionary):
-		if not ok:
-			_on_join_back_pressed()
+	var on_created = func(error):
+		if error:
+			print("Failed to create lobby: %s" % error)
 			return
-		Settings.net_id = net_id
-		if host and host.has("ip") and host.has("port"):
-			Settings.lobby_host = host.ip
-			Settings.lobby_port = int(host.port)
 		_on_start()
 
-	Registry.join_reply.connect(on_join_reply)
-	Registry.join_room(room.id)
+	SteamManager.lobby_created.connect(on_created)
+	SteamManager.create_lobby()
 
-func _on_join_pressed() -> void:
-	Registry.rooms_updated.connect(_set_rooms)
-	var ok := await Registry.ensure_connected(Settings.get_registry_url())
-	if not ok:
-		print("Error: failed to connect to registry")
-		return
+func _on_join_room_pressed(room) -> void:
 	Settings.is_host = false
-	$Join.visible = true
-	$Main.visible = false
-	print("Registry.rooms", Registry.rooms)
-	_set_rooms(Registry.rooms)
-	# _on_start()
+	var on_join_reply = func(error):
+		if error:
+			print("Failed to join lobby: %s" % error)
+			_on_join_back_pressed()
+			return
+		_on_start()
+
+	SteamManager.lobby_joined.connect(on_join_reply)
+	SteamManager.join_lobby(room.id)
+
+func _on_join_back_pressed() -> void:
+	SteamManager.lobby_match_list_updated.disconnect(_set_rooms)
+	JoinSection.visible = false
+	MainSection.visible = true
 
 func _set_rooms(rooms: Array) -> void:
 	var no_rooms := len(rooms) == 0
-	$Join/Searching.visible = no_rooms
+	JoinSearching.visible = no_rooms
 
-	for child in $Join/List.get_children():
-		$Join/List.remove_child(child)
+	for child in JoinLobbyList.get_children():
+		JoinLobbyList.remove_child(child)
 
 	for room in rooms:
 		var button = Button.new()
-		button.text = room.id
-		button.pressed.connect(func(): _on_join_room(room))
-		$Join/List.add_child(button)
-
-func _on_join_back_pressed() -> void:
-	$Join.visible = false
-	$Main.visible = true
+		button.text = "%s (%s)" % [room.name, room.num_members]
+		button.pressed.connect(func(): _on_join_room_pressed(room))
+		JoinLobbyList.add_child(button)
 
 func _on_start() -> void:
-	get_tree().change_scene_to_packed(game_scene)
+	get_tree().change_scene_to_packed(world_scene)

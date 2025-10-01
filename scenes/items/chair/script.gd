105af6f7e107b9e7a3bb5002a737dd6724e626af
Artem Prokop <artem13.prokop@gmail.com>
2025-10-02 00:03:38 +0200

    feat: don't stun self

--- scenes/items/chair/script.gd
+++ scenes/items/chair/script.gd
@@ -10,7 +10,8 @@ extends Interactable
 @export var enemy_stun_time: float = 1.0
 
 const THROW_SPEED := 12.0
-var can_stun := false
+@export var can_stun := false
+var used_by_peer_id: int = -1
 
 func get_outline_target() -> MeshInstance3D:
 	return mesh
@@ -21,36 +22,41 @@ func get_is_static() -> bool:
 func get_hunter_can_interact() -> bool:
 	return false
 
-func get_can_stun() -> bool:
-	return can_stun and chair.linear_velocity.length() >= min_stun_speed
+func get_can_stun(peer_id: int) -> bool:
+	return can_stun and chair.linear_velocity.length() >= min_stun_speed and used_by_peer_id != peer_id
 
 func on_stun() -> void:
 	queue_free()
 
 func _physics_process(_delta: float) -> void:
-	if chair.linear_velocity.length() < min_stun_speed:
+	if chair.linear_velocity.length() < min_stun_speed and can_stun:
 		can_stun = false
+		used_by_peer_id = -1
 
 # Enable = pick up, Disable = throw
 func perform_interact(enable: bool, metadata: Dictionary):
 	var hand: Node3D = metadata.get("hand")
+	var target: CharacterBody3D = metadata.get("target")
+	var peer_id: int = metadata.get("peer_id")
 	if hand == null:
 		return
 
 	if enable:
+		if target != null:
+			used_by_peer_id = peer_id
+			add_collision_exception_with(target)
 		get_parent().remove_child(self)
 		hand.add_child(self)
 		transform = Transform3D(Basis.from_euler(Vector3(0.0, deg_to_rad(-90.0), deg_to_rad(-90.0))), Vector3(0.35, 0, -0.7))
-		chair.freeze = true
-		chair.collision_layer = 0
+		chair.collision_layer = 1 << 0
 	else:
+		remove_collision_exception_with(target)
 		var world := get_tree().current_scene
 		var t := global_transform
 		get_parent().remove_child(self)
 		world.add_child(self)
 		global_transform = t
 
-		chair.freeze = false
 		chair.collision_layer = collision_layer_enabled
 		var throw_direction: Vector3 = metadata.get("direction", Vector3.ZERO)
 		if throw_direction != Vector3.ZERO:

36c5094fbbe2d1565c73c82958f3d3fd2fe05edb
Artem Prokop <artem13.prokop@gmail.com>
2025-10-19 22:41:41 +0200

    fix: door to chair interaction

--- scenes/door/script.gd
+++ scenes/door/script.gd
@@ -5,10 +5,11 @@ const ANIMATION_DURATION = 0.45
 
 var is_opened = false
 
-@export var door: MeshInstance3D
+@export var mesh: MeshInstance3D
+@export var body: AnimatableBody3D
 
 func get_outline_target() -> MeshInstance3D:
-	return door
+	return mesh
 
 func get_is_static() -> bool:
 	return true
@@ -18,8 +19,10 @@ func get_hunter_can_interact() -> bool:
 
 func perform_interact(enable: bool, metadata: Dictionary) -> void:
 	var player_position: Vector3 = metadata.get("position", Vector3.ZERO)
-	var door_position: Vector3 = door.global_transform.origin
+	var door_position: Vector3 = body.global_transform.origin
 	var axis_value: float = (player_position.z - door_position.z) if ORIENTATION == "horizontal" else (player_position.x - door_position.x)
 	var target: float = 0.0 if is_opened else signf(axis_value) * 90.0 if enable else 0.0
-	get_tree().create_tween().tween_property(door, "rotation_degrees:y", target, ANIMATION_DURATION)
+	
+	var tween := get_tree().create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
+	tween.tween_property(body, "rotation_degrees:y", target, ANIMATION_DURATION)
 	is_opened = not is_opened

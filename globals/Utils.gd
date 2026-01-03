90bf6695c530805dd6b34d56be9d9362178d35c1
chelokot <andrii@oriane.xyz>
2026-01-04 01:23:01 +0200

    chore: refactor smooth_damp_vector3 to use exp_cubic_approx function

--- globals/Utils.gd
+++ globals/Utils.gd
@@ -9,13 +9,21 @@ func resolve_node(value):
 		return value
 	return null
 
+const EXP_APPROX_CONSTANT_COEFF: float = 1.0
+const EXP_APPROX_LINEAR_COEFF: float = 1.0
+const EXP_APPROX_QUADRATIC_COEFF: float = 0.48
+const EXP_APPROX_CUBIC_COEFF: float = 0.235
+
+func exp_cubic_approx(value: float) -> float:
+	return EXP_APPROX_CONSTANT_COEFF / (EXP_APPROX_CONSTANT_COEFF + EXP_APPROX_LINEAR_COEFF * value + EXP_APPROX_QUADRATIC_COEFF * value * value + EXP_APPROX_CUBIC_COEFF * value * value * value)
+
 func smooth_damp_vector3(current: Vector3, target: Vector3, velocity: Vector3, smooth_time: float, delta: float) -> Array[Vector3]:
 	var clamped_smooth_time: float = maxf(smooth_time, 0.0001)
 	var omega: float = 2.0 / clamped_smooth_time
-	var x: float = omega * delta
-	var exp: float = 1.0 / (1.0 + x + 0.48 * x * x + 0.235 * x * x * x)
+	var scaled_time: float = omega * delta
+	var damping_factor: float = exp_cubic_approx(scaled_time)
 	var change: Vector3 = current - target
 	var temp: Vector3 = (velocity + omega * change) * delta
-	var new_velocity: Vector3 = (velocity - omega * temp) * exp
-	var new_value: Vector3 = target + (change + temp) * exp
+	var new_velocity: Vector3 = (velocity - omega * temp) * damping_factor
+	var new_value: Vector3 = target + (change + temp) * damping_factor
 	return [new_value, new_velocity]
